a0e3a42be9c145e834b1bb9c8a19c42bdd107d73
Prog-Vinsu <tognerepoloniniv@gmail.com>
2026-01-17 10:32:37 -0300

    feat: adding initial layout and setting color variables

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,81 +1,324 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+    <q-header class="bg-white text-grey-8 q-py-sm" style="border-bottom: 1px solid #e0e0e0;">
+      <q-toolbar class="container">
+        <q-btn
+          v-if="isMobile"
+          flat
+          round
+          dense
+          size="lg"
+          color="grey-6"
+          icon="menu"
+          @click="toggleSidebar"
+          class="q-mr-sm"
+        />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title
+          shrink
+          :class="{ 'absolute-center': isMobile, 'q-mr-md': !isMobile }"
+        >
+          <div class="row items-center cursor-pointer" @click="$router.push('/')">
+            <q-icon
+                name="account_balance"
+                color="primary"
+                :size="isMobile ? '28px' : '32px'"
+                class="q-mr-sm"
+            />
+            <div class="column">
+              <span
+                class="text-caption text-uppercase text-weight-bold text-primary"
+                :style="{ fontSize: isMobile ? '10px' : '12px', lineHeight: '1.1' }"
+              >
+                Prefeitura
+              </span>
+              <span
+                class="text-caption text-uppercase text-primary"
+                :style="{ fontSize: isMobile ? '10px' : '12px', lineHeight: '1.1' }"
+              >
+                De Jaguaré-ES
+              </span>
+            </div>
+          </div>
+        </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <template v-if="!isMobile">
+            <q-space />
+            <div class="row q-gutter-x-lg">
+              <q-btn
+                v-for="item in menuList"
+                :key="item.path"
+                flat
+                no-caps
+                :label="item.label"
+                :to="item.path"
+                exact
+                class="text-grey-8 text-weight-regular"
+                active-class="item-ativo"
+              />
+            </div>
+            <q-space />
+        </template>
+
+        <div class="row items-center q-gutter-x-md" :class="{ 'q-ml-auto': isMobile }">
+          <template v-if="!isMobile">
+              <q-btn flat no-caps>
+                <span class="text-grey-8">Entrar</span>
+                <q-icon right name="perm_identity" color="primary" />
+              </q-btn>
+              <q-separator vertical inset />
+          </template>
+
+          <q-btn
+            flat
+            round
+            color="primary"
+            icon="search"
+            :class="{ 'text-grey-6': isMobile }"
+            :size="isMobile ? 'lg' : 'md'"
+          />
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      side="left"
+      overlay
+      behavior="mobile"
+      class="bg-white"
+      :width="280"
+    >
+      <div class="column full-height">
+        <div class="q-pa-md row items-center justify-between bg-grey-1">
+           <span class="text-weight-bold text-grey-8">MENU</span>
+           <q-btn flat round icon="close" size="sm" color="grey-7" @click="toggleSidebar" />
+        </div>
 
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
+        <q-separator />
+
+        <q-list padding class="text-grey-8">
+          <q-item
+            v-for="item in menuList"
+            :key="item.path"
+            clickable
+            v-ripple
+            :to="item.path"
+            active-class="text-primary bg-blue-1 text-weight-bold"
+          >
+            <q-item-section avatar>
+              <q-icon :name="item.icon" />
+            </q-item-section>
+            <q-item-section>
+              {{ item.label }}
+            </q-item-section>
+          </q-item>
+        </q-list>
+
+        <div class="q-mt-auto q-pa-md">
+           <q-btn outline color="primary" class="full-width" label="Entrar" icon="perm_identity" />
+        </div>
+      </div>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container class="bg-background-page">
       <router-view />
+
+      <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[24, 24]">
+        <q-btn fab icon="keyboard_arrow_up" color="white" text-color="grey-8" class="shadow-3" />
+      </q-page-scroller>
     </q-page-container>
+
+    <q-footer class="bg-white text-grey-8">
+      <div class="bg-primary-light q-py-lg">
+        <div class="container flex items-center" :class="isMobile ? 'column justify-center text-center q-gutter-y-md' : 'row justify-between'">
+            <div class="text-h5 text-weight-bold text-grey-9" style="line-height: 1.2;">
+              Experimente o aplicativo<br v-if="isMobile"> de Jaguaré!
+            </div>
+
+            <div class="row q-gutter-sm items-center">
+              <a href="https://apple.com/br/app-store" target="_blank" class="app-link">
+                <img
+                  src="/src/assets/serviceImages/appStore.png"
+                  alt="Baixar na App Store"
+                  :style="{ height: isMobile ? '38px' : '48px' }"
+                >
+              </a>
+              <a href="https://play.google.com/store" target="_blank" class="app-link">
+                <img
+                  src="/src/assets/serviceImages/GooglePlay.png"
+                  alt="Disponível no Google Play"
+                  :style="{ height: isMobile ? '50px' : '61px' }"
+                >
+              </a>
+            </div>
+
+        </div>
+      </div>
+
+      <div class="container q-py-xl">
+        <div v-if="isMobile" class="column items-center text-center q-gutter-y-lg">
+           <div class="row items-center justify-center">
+              <q-icon name="account_balance" color="primary" size="32px" class="q-mr-sm" />
+              <div class="column items-start">
+                <span class="text-caption text-uppercase text-weight-bold text-primary">Prefeitura</span>
+                <span class="text-caption text-uppercase text-primary">De Jaguaré-ES</span>
+              </div>
+           </div>
+
+           <div class="row q-gutter-md">
+             <q-btn round flat color="primary" class="bg-green-1" icon="facebook" />
+             <q-btn round flat color="primary" class="bg-green-1" icon="camera_alt" />
+             <q-btn round flat color="primary" class="bg-green-1" icon="smart_display" />
+             <q-btn round flat color="primary" class="bg-green-1" icon="language" />
+           </div>
+
+           <div class="column items-center">
+              <div class="text-weight-bold q-mb-sm">Telefone 24 horas:</div>
+              <div class="row q-mb-md">
+                <div>
+                  <q-icon name="phone" color="primary" size="xs" class="q-mr-xs" />
+                  <span class="text-weight-bold">162</span>
+                  <div class="column items-center">
+                    <span class="q-ml-xs text-grey-6 text-caption">Capital</span>
+                  </div>
+                </div>
+                <q-separator vertical class="q-mx-md" style="height: 15px" />
+                <div>
+                  <q-icon name="phone" color="primary" size="xs" class="q-mr-xs" />
+                  <span class="text-weight-bold">0800.283.9059</span>
+                  <div class="column">
+                    <span class="q-ml-xs text-grey-6 text-caption">Outras localidades</span>
+                  </div>
+                </div>
+              </div>
+           </div>
+
+           <q-separator class="full-width" />
+           <div class="text-caption text-grey-5">Um produto <span class="text-weight-bold text-grey-7">UpCities</span></div>
+        </div>
+
+        <div v-else class="row q-col-gutter-lg">
+          <div class="col-12 col-md-3">
+             <div class="row items-center q-mb-md">
+                <q-icon name="account_balance" color="primary" size="28px" class="q-mr-sm" />
+                <div class="column">
+                  <span class="text-caption text-uppercase text-weight-bold text-primary">Prefeitura</span>
+                  <span class="text-caption text-uppercase text-primary">De Jaguaré-ES</span>
+                </div>
+             </div>
+             <div class="row q-gutter-sm q-mt-md">
+               <q-btn round flat size="sm" color="primary" class="bg-green-1" icon="facebook" />
+               <q-btn round flat size="sm" color="primary" class="bg-green-1" icon="camera_alt" />
+               <q-btn round flat size="sm" color="primary" class="bg-green-1" icon="smart_display" />
+               <q-btn round flat size="sm" color="primary" class="bg-green-1" icon="language" />
+             </div>
+          </div>
+
+          <div class="col-12 col-md-3 column q-gutter-y-sm text-body2">
+             <a href="#" class="text-grey-8 text-no-decoration hover-underline">Sobre a Central de Serviços</a>
+             <a href="#" class="text-grey-8 text-no-decoration hover-underline">Estatísticas</a>
+          </div>
+
+          <div class="col-12 col-md-3 column q-gutter-y-sm text-body2">
+             <a href="#" class="text-grey-8 text-no-decoration hover-underline">Serviços</a>
+             <a href="#" class="text-grey-8 text-no-decoration hover-underline">Minhas Solicitações</a>
+             <a href="#" class="text-grey-8 text-no-decoration hover-underline">Dúvidas</a>
+          </div>
+
+          <div class="col-12 col-md-3 text-body2">
+             <div class="text-weight-bold q-mb-md">Telefone 24 horas:</div>
+             <div class="row  q-gutter-x-lg">
+               <div class="row">
+                 <q-icon name="phone" color="primary" class="q-mr-sm" />
+                 <div>
+                   <div class="text-weight-bold text-body2">162</div>
+                   <div class="text-grey-6">Capital</div>
+                 </div>
+               </div>
+               <q-separator vertical />
+               <div class="row">
+                 <q-icon name="phone" color="primary" class="q-mr-sm" />
+                 <div>
+                   <div class="text-weight-bold text-body2">0800.283.9059</div>
+                   <div class="text-grey-6">Outras localidades</div>
+                 </div>
+               </div>
+             </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div v-if="!isMobile" class="text-center q-py-sm text-caption text-grey-5 border-top-light">
+          Um produto <span class="text-weight-bold text-grey-7">UpCities</span>
+      </div>
+
+    </q-footer>
+
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+import { ref, computed } from 'vue'
+import { useQuasar } from 'quasar'
 
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
+const $q = useQuasar()
+const isMobile = computed(() => $q.screen.lt.md)
 
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
+const leftDrawerOpen = ref(false)
+function toggleSidebar() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
 }
+
+const menuList = [
+  { label: 'Agendar', path: '/agendar', icon: 'event' },
+  { label: 'Meus Agendamentos', path: '/meus-agendamentos', icon: 'list_alt' },
+  { label: 'Ajuda', path: '/faq', icon: 'help_outline' }
+]
 </script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  width: 100%;
+  padding-left: 16px;
+  padding-right: 16px;
+}
+
+.item-ativo {
+  position: relative;
+  color: #000 !important;
+  font-weight: bold !important;
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 3px;
+    background-color: #3a3a3a;
+  }
+}
+
+.text-no-decoration {
+  text-decoration: none;
+}
+.hover-underline:hover {
+  text-decoration: underline;
+}
+.border-top-light {
+    border-top: 1px solid #eee;
+}
+
+.bg-primary-light {
+  background-color: $primary-light !important;
+}
+
+.bg-background-page {
+  background-color: $background-page !important;
+}
+</style>
